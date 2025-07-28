efeea6a1aa154c9f2032d1a6438a2028261ff396
Robert Haynes <robhaynes0420@gmail.com>
2025-07-28 17:43:50 +0100

    Exposed nested file to path

--- racecards/upload.py
+++ racecards/upload.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from supabase_funcs.upload import upload_df_to_supabase
+import sys
+sys.path.append('../')
 
 df = pd.read_pickle("output/racecards.pkl")
 upload_df_to_supabase(df, "racecards")
