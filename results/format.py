656dbe5dbc4d676d4276d1bcdf5d4b173f308001
Robert Haynes <robhaynes0420@gmail.com>
2025-07-28 16:03:19 +0100

    Include draw in data

--- results/format.py
+++ results/format.py
@@ -15,12 +15,14 @@ for result in results_response['results']:
         {
             'race_id': result['race_id'],
             'horse_id': runner['horse_id'],
+            'draw': runner['draw'],
             'position': runner['position']
         }
         for runner in result['runners']
     ])
 
     results_df['position'] = pd.to_numeric(results_df['position'], errors='coerce')
+    results_df['draw'] = pd.to_numeric(results_df['draw'], errors='coerce')
 
     final_results = pd.concat([final_results, results_df], ignore_index=True)
 
